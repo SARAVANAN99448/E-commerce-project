754ede2ce4b0e70e1858de14b8cf3dc7362c32b0
SARAVANAN99448 <saravanasaro0000@gmail.com>
2024-10-11 08:08:25 +0530

    updated

--- frontend/src/components/Newarrivals.jsx
+++ frontend/src/components/Newarrivals.jsx
@@ -3,24 +3,33 @@ import product2 from "../assets/images/product2.png"
 import product3 from "../assets/images/product3.png"
 import product4 from "../assets/images/product4.png"
 const Newarrivals = () => {
+    
 
     const config = {
-        products :[
-            {image:product1},{image:product2},{image:product3},{image:product4}
+        products: [
+            { image: product1, text: "T-shirt with Tape Details",dollar:"$120",discount :"", star:<><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star-half text-yellow-400" ></i></>},
+            { image: product2, text: "Skinny Fit Jeans",dollar:"$240 ",discount :"$260", star:<><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star-half text-yellow-400" ></i></>},
+            { image: product3, text: "checkered Shirt",dollar:"$180",discount :"", star:<><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star-half text-yellow-400" ></i></>},
+            { image: product4, text: "Sleeve Striped T-shirt",dollar:"$130",discount :"$160", star:<><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star text-yellow-400" ></i><i className="mr-1 fa-solid fa-star-half text-yellow-400" ></i></>}
         ]
     }
     return (<>
-        <section>
-            <div className="text-center">
-                <h1 className="font-extrabold text-3xl">NEW ARRIVALS</h1>
-                <div className="flex flex-wrap gap-5 justify-center mt-10">
-                {
-                    config.products.map(function(data){
-                        return(
-                            <img src={data.image} alt=""  className="w-[300px] h-[300px] rounded-lg"/>
-                        )
-                    })
-                }
+        <section className="flex items-center justify-center">
+            <div className="">
+                <h1 className="font-extrabold text-3xl text-center ">NEW ARRIVALS</h1>
+                <div className=" grid md:grid-cols-4 grid-cols-2 gap-5  mt-10 md:mx-auto mx-5">
+                    {
+                        config.products.map(function (data) {
+                            return (
+                                <div>
+                                    <img src={data.image} alt="" className="w-[280px] h-[300px] rounded-3xl" />
+                                    <h1 className="ml-3 font-semibold mt-3 mb-2 text-xl">{data.text}</h1>
+                                    <p className="ml-3">{data.star}</p>
+                                    <p className="ml-3 font-semibold text-xl">{data.dollar}  <span className="font-light line-through ">{data.discount}</span></p>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </section>
